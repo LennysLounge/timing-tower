f39523f88f501b70736e54ae83e458b1606fddb2
Leonard Schüngel <schuengel.leonard@gmail.com>
2023-11-27 00:37:41 +0100

    extract the editor camera into its own module

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -1,18 +1,10 @@
+pub mod camera;
+
 use std::{fs::File, io::Write};
 
-use bevy::{
-    input::{
-        mouse::{MouseButtonInput, MouseWheel},
-        ButtonState,
-    },
-    math::{vec2, vec3},
-    prelude::{
-        resource_exists, AssetEvent, AssetServer, Camera, Camera2dBundle, Commands, Component,
-        EventReader, Image, IntoSystemConfigs, Plugin, Query, Res, ResMut, Resource, Startup,
-        Transform, UVec2, Update, Vec2, With,
-    },
-    render::camera::Viewport,
-    window::{PrimaryWindow, Window},
+use bevy::prelude::{
+    resource_exists, AssetEvent, AssetServer, EventReader, Image, IntoSystemConfigs, Plugin, Query,
+    Res, ResMut, Resource, Startup, Update, With,
 };
 use bevy_egui::{
     egui::{self, Rect, ScrollArea, Ui},
@@ -30,101 +22,29 @@ use crate::{
     MainCamera,
 };
 
+use self::camera::{EditorCamera, EditorCameraPlugin};
+
 pub struct EditorPlugin;
 impl Plugin for EditorPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(Startup, setup)
+        app.add_plugins(EditorCameraPlugin)
+            .add_systems(Startup, setup)
             .add_systems(
                 Update,
-                (
-                    ui.run_if(resource_exists::<EditorState>()),
-                    set_camera_viewport,
-                )
-                    .chain(),
+                ui.run_if(resource_exists::<EditorState>())
+                    .before(camera::set_camera_viewport),
             )
-            .add_systems(Update, camera_drag)
             .add_systems(Update, update_asset_load_state);
     }
 }
 
-fn setup(mut commands: Commands, mut ctx: EguiContexts) {
+fn setup(mut ctx: EguiContexts) {
     dear_egui::set_theme(ctx.ctx_mut(), dear_egui::SKY);
-
-    commands.spawn((Camera2dBundle::default(), MainCamera, CameraDrag::new()));
-}
-
-#[derive(Component)]
-struct CameraDrag {
-    drag_position: Option<Vec2>,
-    scale: f32,
-}
-impl CameraDrag {
-    fn new() -> Self {
-        Self {
-            drag_position: None,
-            scale: 1.0,
-        }
-    }
-}
-
-fn camera_drag(
-    mut mouse_events: EventReader<MouseButtonInput>,
-    mut scroll_events: EventReader<MouseWheel>,
-    window: Query<&Window, With<PrimaryWindow>>,
-    mut camera: Query<(&mut CameraDrag, &mut Transform, &Camera), With<MainCamera>>,
-) {
-    let window = window.single();
-
-    let (mut camera_drag, mut camera_transform, camera) = camera.single_mut();
-
-    for ev in mouse_events.read() {
-        match ev.state {
-            ButtonState::Pressed => {
-                let is_cursor_inside_viewport = |cursor_pos: &Vec2| {
-                    camera.viewport.as_ref().is_some_and(|viewport| {
-                        let viewport_max = (viewport.physical_position + viewport.physical_size)
-                            .as_vec2()
-                            - vec2(5.0, 5.0);
-                        let viewport_min = viewport.physical_position.as_vec2() + vec2(5.0, 5.0);
-
-                        viewport_max.x > cursor_pos.x
-                            && viewport_max.y > cursor_pos.y
-                            && viewport_min.x < cursor_pos.x
-                            && viewport_min.y < cursor_pos.y
-                    })
-                };
-                camera_drag.drag_position =
-                    window.cursor_position().filter(is_cursor_inside_viewport);
-            }
-            ButtonState::Released => {
-                camera_drag.drag_position = None;
-            }
-        }
-    }
-
-    for ev in scroll_events.read() {
-        if ev.y > 0.0 {
-            camera_drag.scale *= 0.9;
-        }
-        if ev.y < 0.0 {
-            camera_drag.scale /= 0.9;
-        }
-        camera_transform.scale = vec3(camera_drag.scale, camera_drag.scale, 1.0);
-    }
-
-    if let Some(drag_position) = camera_drag.drag_position {
-        if let Some(cursor_position) = window.cursor_position() {
-            let delta = drag_position - cursor_position;
-            camera_transform.translation += vec3(delta.x, -delta.y, 0.0) * camera_drag.scale;
-        }
-        camera_drag.drag_position = window.cursor_position();
-    }
 }
 
 #[derive(Resource)]
 pub struct EditorState {
     dock_state: DockState<Tab>,
-    viewport: Rect,
     selected_node: Option<Uuid>,
 }
 impl EditorState {
@@ -141,7 +61,6 @@ impl EditorState {
         Self {
             selected_node: None,
             dock_state: state,
-            viewport: Rect::NOTHING,
         }
     }
 }
@@ -211,7 +130,7 @@ fn ui(
     mut state: ResMut<EditorState>,
     mut style: ResMut<StyleDefinition>,
     mut variable_repo: ResMut<AssetRepo>,
-    camera_drag: Query<&CameraDrag, With<MainCamera>>,
+    mut editor_camera: Query<&mut EditorCamera, With<MainCamera>>,
     asset_server: Res<AssetServer>,
 ) {
     egui::TopBottomPanel::top("Top panel").show(ctx.ctx_mut(), |ui| {
@@ -226,16 +145,16 @@ fn ui(
     });
     egui::TopBottomPanel::bottom("Bottom panel").show(ctx.ctx_mut(), |ui| {
         ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-            let zoom = 1.0 / camera_drag.single().scale * 100.0;
+            let zoom = 1.0 / editor_camera.single().scale * 100.0;
             ui.label(format!("Zoom: {:.0}%", zoom));
         });
     });
 
     let EditorState {
         dock_state,
-        viewport,
         selected_node,
     } = &mut *state;
+    let viewport = &mut editor_camera.single_mut().raw_viewport;
     DockArea::new(dock_state)
         .style(egui_dock::Style::from_egui(ctx.ctx_mut().style().as_ref()))
         .show(
@@ -400,31 +319,6 @@ fn property_editor(
             .into_iter()
             .for_each(|a| a.load_asset(&*asset_server));
     }
-}
-
-// make camera only render to view not obstructed by UI
-fn set_camera_viewport(
-    state: Res<EditorState>,
-    primary_window: Query<&mut Window, With<PrimaryWindow>>,
-    egui_settings: Res<bevy_egui::EguiSettings>,
-    mut cameras: Query<&mut Camera, With<MainCamera>>,
-) {
-    let mut cam = cameras.single_mut();
-
-    let Ok(window) = primary_window.get_single() else {
-        return;
-    };
-
-    let scale_factor = window.scale_factor() * egui_settings.scale_factor;
-
-    let viewport_pos = state.viewport.left_top().to_vec2() * scale_factor as f32;
-    let viewport_size = state.viewport.size() * scale_factor as f32;
-
-    cam.viewport = Some(Viewport {
-        physical_position: UVec2::new(viewport_pos.x as u32, viewport_pos.y as u32),
-        physical_size: UVec2::new(viewport_size.x as u32, viewport_size.y as u32),
-        depth: 0.0..1.0,
-    });
 }
 
 fn update_asset_load_state(
---
+++ crates/editor/src/editor/camera.rs
@@ -0,0 +1,128 @@
+use bevy::{
+    app::{Plugin, PostUpdate, Startup, Update},
+    core_pipeline::core_2d::Camera2dBundle,
+    ecs::{
+        component::Component,
+        event::EventReader,
+        query::With,
+        system::{Commands, Query, Res},
+    },
+    input::{
+        mouse::{MouseButtonInput, MouseWheel},
+        ButtonState,
+    },
+    math::{vec2, vec3, UVec2, Vec2},
+    render::camera::{Camera, Viewport},
+    transform::components::Transform,
+    window::{PrimaryWindow, Window},
+};
+use bevy_egui::egui::Rect;
+
+use crate::MainCamera;
+
+pub struct EditorCameraPlugin;
+impl Plugin for EditorCameraPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_systems(Startup, setup)
+            .add_systems(Update, camera_drag)
+            .add_systems(PostUpdate, set_camera_viewport);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Camera2dBundle::default(), MainCamera, EditorCamera::new()));
+}
+
+#[derive(Component)]
+pub struct EditorCamera {
+    drag_position: Option<Vec2>,
+    pub scale: f32,
+    pub raw_viewport: Rect,
+}
+impl EditorCamera {
+    fn new() -> Self {
+        Self {
+            drag_position: None,
+            scale: 1.0,
+            raw_viewport: Rect::NOTHING,
+        }
+    }
+}
+
+fn camera_drag(
+    mut mouse_events: EventReader<MouseButtonInput>,
+    mut scroll_events: EventReader<MouseWheel>,
+    window: Query<&Window, With<PrimaryWindow>>,
+    mut camera: Query<(&mut EditorCamera, &mut Transform, &Camera), With<MainCamera>>,
+) {
+    let window = window.single();
+
+    let (mut camera_drag, mut camera_transform, camera) = camera.single_mut();
+
+    for ev in mouse_events.read() {
+        match ev.state {
+            ButtonState::Pressed => {
+                let is_cursor_inside_viewport = |cursor_pos: &Vec2| {
+                    camera.viewport.as_ref().is_some_and(|viewport| {
+                        let viewport_max = (viewport.physical_position + viewport.physical_size)
+                            .as_vec2()
+                            - vec2(5.0, 5.0);
+                        let viewport_min = viewport.physical_position.as_vec2() + vec2(5.0, 5.0);
+
+                        viewport_max.x > cursor_pos.x
+                            && viewport_max.y > cursor_pos.y
+                            && viewport_min.x < cursor_pos.x
+                            && viewport_min.y < cursor_pos.y
+                    })
+                };
+                camera_drag.drag_position =
+                    window.cursor_position().filter(is_cursor_inside_viewport);
+            }
+            ButtonState::Released => {
+                camera_drag.drag_position = None;
+            }
+        }
+    }
+
+    for ev in scroll_events.read() {
+        if ev.y > 0.0 {
+            camera_drag.scale *= 0.9;
+        }
+        if ev.y < 0.0 {
+            camera_drag.scale /= 0.9;
+        }
+        camera_transform.scale = vec3(camera_drag.scale, camera_drag.scale, 1.0);
+    }
+
+    if let Some(drag_position) = camera_drag.drag_position {
+        if let Some(cursor_position) = window.cursor_position() {
+            let delta = drag_position - cursor_position;
+            camera_transform.translation += vec3(delta.x, -delta.y, 0.0) * camera_drag.scale;
+        }
+        camera_drag.drag_position = window.cursor_position();
+    }
+}
+
+// make camera only render to view not obstructed by UI
+pub fn set_camera_viewport(
+    primary_window: Query<&mut Window, With<PrimaryWindow>>,
+    egui_settings: Res<bevy_egui::EguiSettings>,
+    mut cameras: Query<(&mut Camera, &EditorCamera), With<MainCamera>>,
+) {
+    let (mut cam, state) = cameras.single_mut();
+
+    let Ok(window) = primary_window.get_single() else {
+        return;
+    };
+
+    let scale_factor = window.scale_factor() * egui_settings.scale_factor;
+
+    let viewport_pos = state.raw_viewport.left_top().to_vec2() * scale_factor as f32;
+    let viewport_size = state.raw_viewport.size() * scale_factor as f32;
+
+    cam.viewport = Some(Viewport {
+        physical_position: UVec2::new(viewport_pos.x as u32, viewport_pos.y as u32),
+        physical_size: UVec2::new(viewport_size.x as u32, viewport_size.y as u32),
+        depth: 0.0..1.0,
+    });
+}

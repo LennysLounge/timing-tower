91c2949f082857a008207622e5f1d12b66606c0b
Leonard Schüngel <schuengel.leonard@gmail.com>
2023-12-02 15:10:13 +0100

    Change the SetStyle event to use Uuid

--- crates/common/src/cell/style.rs
+++ crates/common/src/cell/style.rs
@@ -1,5 +1,6 @@
-use bevy::prelude::{Color, Entity, Event, Handle, Image, Vec2, Vec3};
+use bevy::prelude::{Color, Entity, Event, Vec2, Vec3};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Event)]
 pub struct SetStyle {
@@ -14,7 +15,7 @@ pub struct CellStyle {
     pub text_alignment: TextAlignment,
     pub text_position: Vec2,
     pub color: Color,
-    pub texture: Option<Handle<Image>>,
+    pub texture: Option<Uuid>,
     pub pos: Vec3,
     pub size: Vec2,
     pub skew: f32,

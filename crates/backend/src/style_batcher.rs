56085f130b35792e45f38c668f6d922a596b0233
Leonard Schüngel <schuengel.leonard@gmail.com>
2023-12-15 00:18:41 +0100

    Remove dead cells from the batcher

--- crates/backend/src/style_batcher.rs
+++ crates/backend/src/style_batcher.rs
@@ -4,10 +4,13 @@
 //! The batcher only sends out styles that changed. It does so by comparing the incomming
 //! style with the cell style from the last frame and only outputs the changes.
 
-use std::sync::{Arc, Weak};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Weak},
+};
 
 use bevy::{
-    app::{First, Plugin},
+    app::{First, Plugin, PostUpdate},
     ecs::system::{ResMut, Resource},
 };
 use common::communication::{CellStyle, StyleCommand};
@@ -17,18 +20,22 @@ pub struct StyleBatcherPlugin;
 impl Plugin for StyleBatcherPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.init_resource::<StyleBatcher>()
-            .add_systems(First, clear_style_batcher);
+            .add_systems(First, clear_style_batcher)
+            .add_systems(PostUpdate, prepare_batcher);
     }
 }
 
 #[derive(Resource, Default)]
 pub struct StyleBatcher {
+    last_styles: HashMap<Uuid, (Weak<()>, CellStyle)>,
     commands: Vec<StyleCommand>,
 }
 impl StyleBatcher {
     /// Add a style to the batch
     pub fn add(&mut self, cell_id: &CellId, style: CellStyle) {
         // TODO: implement the change detection
+        self.last_styles
+            .insert(cell_id.id, (cell_id.weak(), style.clone()));
         self.commands.push(StyleCommand {
             id: cell_id.id,
             style,
@@ -44,6 +51,21 @@ fn clear_style_batcher(mut batcher: ResMut<StyleBatcher>) {
     batcher.drain();
 }
 
+fn prepare_batcher(mut batcher: ResMut<StyleBatcher>) {
+    // Test all known cells if they are still alive and remove the dead ones.
+    let dead_cells: Vec<_> = batcher
+        .last_styles
+        .iter()
+        .filter_map(|(id, (reference, _))| (reference.strong_count() == 0).then_some(*id))
+        .collect();
+
+    dead_cells
+        .iter()
+        .for_each(|cell_id| _ = batcher.last_styles.remove(cell_id));
+
+    // TODO: Add remove cell command.
+}
+
 /// Identifies a cell by a unique id.
 pub struct CellId {
     id: Uuid,
@@ -56,12 +78,20 @@ impl CellId {
             alive: Arc::new(()),
         }
     }
+    /// Id of the cell.
     pub fn id(&self) -> &Uuid {
         &self.id
     }
-
     /// Returns a weak pointer to keep track of the aliveness of this CellId.
     pub fn weak(&self) -> Weak<()> {
         Arc::downgrade(&self.alive)
     }
+
+    // /// Returns a weak pointer to keep track of the aliveness of this CellId.
+    // pub fn get_reference(&self) -> CellIdReference {
+    //     CellIdReference {
+    //         id: self.id,
+    //         alive: Arc::downgrade(&self.alive),
+    //     }
+    // }
 }

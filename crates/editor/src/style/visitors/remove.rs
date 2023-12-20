f3e70b26d3a2468b3cc9716dea4123d66984a78b
Leonard Schüngel <schuengel.leonard@gmail.com>
2023-12-20 17:40:09 +0100

    Refactor for clarity

--- crates/editor/src/style/visitors/remove.rs
+++ crates/editor/src/style/visitors/remove.rs
@@ -15,39 +15,51 @@ pub struct RemovedNode {
 
 pub fn remove_node<V: NodeIteratorMut>(node_id: &Uuid, visitable: &mut V) -> Option<RemovedNode> {
     let mut removed_node = None;
-    visitable.walk_mut(&mut |node: NodeMut, method: Method| match (method, node) {
+    visitable.walk_mut(&mut |node: NodeMut, method: Method| {
+        if let ControlFlow::Break(removed) = remove(node, method, node_id) {
+            removed_node = Some(removed);
+            ControlFlow::Break(())
+        } else {
+            ControlFlow::Continue(())
+        }
+    });
+    removed_node
+}
+
+fn remove(node: NodeMut, method: Method, node_id: &Uuid) -> ControlFlow<RemovedNode> {
+    match (method, node) {
         (Method::Visit, NodeMut::AssetFolder(folder)) => {
             if let Some(index) = folder.content.iter().position(|s| s.id() == node_id) {
-                removed_node =
-                    Some(RemovedNode {
-                        parent_id: folder.id,
-                        node: match folder.content.remove(index) {
-                            backend::style::assets::AssetOrFolder::Asset(a) => Box::new(a),
-                            backend::style::assets::AssetOrFolder::Folder(f) => Box::new(f),
-                        },
-                        position: (index == 0).then_some(DropPosition::First).unwrap_or_else(
-                            || DropPosition::After(*folder.content.get(index - 1).unwrap().id()),
-                        ),
-                    });
-                ControlFlow::Break(())
+                ControlFlow::Break(RemovedNode {
+                    parent_id: folder.id,
+                    node: match folder.content.remove(index) {
+                        backend::style::assets::AssetOrFolder::Asset(a) => Box::new(a),
+                        backend::style::assets::AssetOrFolder::Folder(f) => Box::new(f),
+                    },
+                    position: (index == 0)
+                        .then_some(DropPosition::First)
+                        .unwrap_or_else(|| {
+                            DropPosition::After(*folder.content.get(index - 1).unwrap().id())
+                        }),
+                })
             } else {
                 ControlFlow::Continue(())
             }
         }
         (Method::Visit, NodeMut::VariableFolder(folder)) => {
             if let Some(index) = folder.content.iter().position(|s| s.id() == node_id) {
-                removed_node =
-                    Some(RemovedNode {
-                        parent_id: folder.id,
-                        node: match folder.content.remove(index) {
-                            backend::style::variables::VariableOrFolder::Variable(a) => Box::new(a),
-                            backend::style::variables::VariableOrFolder::Folder(f) => Box::new(f),
-                        },
-                        position: (index == 0).then_some(DropPosition::First).unwrap_or_else(
-                            || DropPosition::After(*folder.content.get(index - 1).unwrap().id()),
-                        ),
-                    });
-                ControlFlow::Break(())
+                ControlFlow::Break(RemovedNode {
+                    parent_id: folder.id,
+                    node: match folder.content.remove(index) {
+                        backend::style::variables::VariableOrFolder::Variable(a) => Box::new(a),
+                        backend::style::variables::VariableOrFolder::Folder(f) => Box::new(f),
+                    },
+                    position: (index == 0)
+                        .then_some(DropPosition::First)
+                        .unwrap_or_else(|| {
+                            DropPosition::After(*folder.content.get(index - 1).unwrap().id())
+                        }),
+                })
             } else {
                 ControlFlow::Continue(())
             }
@@ -55,18 +67,18 @@ pub fn remove_node<V: NodeIteratorMut>(node_id: &Uuid, visitable: &mut V) -> Opt
 
         (Method::Visit, NodeMut::TimingTowerRow(row)) => {
             if let Some(index) = row.columns.iter().position(|s| s.id() == node_id) {
-                removed_node =
-                    Some(RemovedNode {
-                        parent_id: *row.id(),
-                        node: match row.columns.remove(index) {
-                            backend::style::timing_tower::ColumnOrFolder::Column(t) => Box::new(t),
-                            backend::style::timing_tower::ColumnOrFolder::Folder(f) => Box::new(f),
-                        },
-                        position: (index == 0).then_some(DropPosition::First).unwrap_or_else(
-                            || DropPosition::After(*row.columns.get(index - 1).unwrap().id()),
-                        ),
-                    });
-                ControlFlow::Break(())
+                ControlFlow::Break(RemovedNode {
+                    parent_id: *row.id(),
+                    node: match row.columns.remove(index) {
+                        backend::style::timing_tower::ColumnOrFolder::Column(t) => Box::new(t),
+                        backend::style::timing_tower::ColumnOrFolder::Folder(f) => Box::new(f),
+                    },
+                    position: (index == 0)
+                        .then_some(DropPosition::First)
+                        .unwrap_or_else(|| {
+                            DropPosition::After(*row.columns.get(index - 1).unwrap().id())
+                        }),
+                })
             } else {
                 ControlFlow::Continue(())
             }
@@ -74,23 +86,22 @@ pub fn remove_node<V: NodeIteratorMut>(node_id: &Uuid, visitable: &mut V) -> Opt
 
         (Method::Visit, NodeMut::TimingTowerColumnFolder(folder)) => {
             if let Some(index) = folder.content.iter().position(|s| s.id() == node_id) {
-                removed_node =
-                    Some(RemovedNode {
-                        parent_id: *folder.id(),
-                        node: match folder.content.remove(index) {
-                            backend::style::timing_tower::ColumnOrFolder::Column(t) => Box::new(t),
-                            backend::style::timing_tower::ColumnOrFolder::Folder(f) => Box::new(f),
-                        },
-                        position: (index == 0).then_some(DropPosition::First).unwrap_or_else(
-                            || DropPosition::After(*folder.content.get(index - 1).unwrap().id()),
-                        ),
-                    });
-                ControlFlow::Break(())
+                ControlFlow::Break(RemovedNode {
+                    parent_id: *folder.id(),
+                    node: match folder.content.remove(index) {
+                        backend::style::timing_tower::ColumnOrFolder::Column(t) => Box::new(t),
+                        backend::style::timing_tower::ColumnOrFolder::Folder(f) => Box::new(f),
+                    },
+                    position: (index == 0)
+                        .then_some(DropPosition::First)
+                        .unwrap_or_else(|| {
+                            DropPosition::After(*folder.content.get(index - 1).unwrap().id())
+                        }),
+                })
             } else {
                 ControlFlow::Continue(())
             }
         }
         _ => ControlFlow::Continue(()),
-    });
-    removed_node
+    }
 }

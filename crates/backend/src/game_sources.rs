0634a2bd1df2cf32fb058475c3975d1d75d528b4
Leonard Schüngel <schuengel.leonard@gmail.com>
2024-02-19 21:41:35 +0100

    Add more game sources

--- crates/backend/src/game_sources.rs
+++ crates/backend/src/game_sources.rs
@@ -218,20 +218,6 @@ pub fn get_game_sources() -> Vec<&'static GameSource> {
                 //
                 //   Driver
                 //
-                GameSource::new_number(
-                    uuid!("6330a6bb-51d1-4af7-9bd0-efeb00b1ff52"),
-                    "Position",
-                    |_: &ValueStore, context: ModelContext<'_>| {
-                        context.entry.map(|e| *e.position as f32)
-                    },
-                ),
-                GameSource::new_number(
-                    uuid!("171d7438-3179-4c70-b818-811cf86d514e"),
-                    "Car number",
-                    |_: &ValueStore, context: ModelContext<'_>| {
-                        context.entry.map(|e| *e.car_number as f32)
-                    },
-                ),
                 GameSource::new_text(
                     uuid!("8abcf9d5-60f7-4886-a716-139d62ad83ac"),
                     "Driver name",
@@ -249,16 +235,6 @@ pub fn get_game_sources() -> Vec<&'static GameSource> {
                         })
                     },
                 ),
-                GameSource::new_bool(
-                    uuid!("de909160-f54b-40cf-a987-6a8453df0914"),
-                    "Is focused",
-                    |_: &ValueStore, context: ModelContext<'_>| context.entry.map(|e| e.focused),
-                ),
-                GameSource::new_bool(
-                    uuid!("c16f71b9-dcc9-4f04-9579-ea5211fa99be"),
-                    "Is in pits",
-                    |_: &ValueStore, context: ModelContext<'_>| context.entry.map(|e| *e.in_pits),
-                ),
                 GameSource::new_text(
                     uuid!("4507167c-4c78-4686-b7a2-44809d969cee"),
                     "Car name",
@@ -272,6 +248,164 @@ pub fn get_game_sources() -> Vec<&'static GameSource> {
                     |_: &ValueStore, context: ModelContext<'_>| {
                         context.entry.map(|e| e.car.manufacturer().to_owned())
                     },
+                ),
+                GameSource::new_text(
+                    uuid!("fcfa2406-6088-47f4-b5f3-db75488e896d"),
+                    "Car category",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| e.car.category().name.to_owned())
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("171d7438-3179-4c70-b818-811cf86d514e"),
+                    "Car number",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.car_number as f32)
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("6330a6bb-51d1-4af7-9bd0-efeb00b1ff52"),
+                    "Position",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.position as f32)
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("6cd92029-6f37-4c19-b392-9c8f723a1ac5"),
+                    "Spline position",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.spline_pos as f32)
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("30cd7503-df5f-4fcf-9175-e7b6b469b189"),
+                    "Lap count",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.lap_count as f32)
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("1045e491-f5c5-4551-9239-7a79f6a3a8c6"),
+                    "Current lap sec",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| e.current_lap.time.ms as f32 / 1000.0)
+                    },
+                ),
+                GameSource::new_text(
+                    uuid!("b835a422-d98e-4e84-9942-8abf478f0c49"),
+                    "Current lap",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| e.current_lap.time.format())
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("9b42568b-6fbd-4ae3-9290-5151e6336ad4"),
+                    "Best lap sec",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context
+                            .entry
+                            .and_then(|e| e.best_lap.as_ref().as_ref())
+                            .map(|best_lap| best_lap.time.ms as f32 / 1000.0)
+                    },
+                ),
+                GameSource::new_text(
+                    uuid!("db85f061-0d27-4d8c-9acc-44de629760bc"),
+                    "Best lap",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context
+                            .entry
+                            .and_then(|e| e.best_lap.as_ref().as_ref())
+                            .map(|best_lap| best_lap.time.format())
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("0de05742-c101-41d1-88db-57ca293a0ab9"),
+                    "Performance delta sec",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context
+                            .entry
+                            .map(|e| e.performance_delta.ms as f32 / 1000.0)
+                    },
+                ),
+                GameSource::new_text(
+                    uuid!("6d1e55e3-11b4-4792-94c9-facdd7463d7c"),
+                    "Best lap",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| e.performance_delta.format())
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("29081b33-0525-4d4c-b077-87c90680b45f"),
+                    "Time behind leader sec",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context
+                            .entry
+                            .map(|e| e.time_behind_leader.ms as f32 / 1000.0)
+                    },
+                ),
+                GameSource::new_text(
+                    uuid!("bf09365b-0f51-4482-b3c3-1c2c6e273bfc"),
+                    "Time behind leader",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| e.time_behind_leader.format())
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("9f0b3c0c-0379-4808-a99d-d3a416cc8f1c"),
+                    "Time behind position ahead sec",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context
+                            .entry
+                            .map(|e| e.time_behind_position_ahead.ms as f32 / 1000.0)
+                    },
+                ),
+                GameSource::new_text(
+                    uuid!("dba48b29-6fb7-4a24-98e1-9066c7cde3b6"),
+                    "Time behind position ahead",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| e.time_behind_position_ahead.format())
+                    },
+                ),
+                GameSource::new_bool(
+                    uuid!("c16f71b9-dcc9-4f04-9579-ea5211fa99be"),
+                    "Is in pits",
+                    |_: &ValueStore, context: ModelContext<'_>| context.entry.map(|e| *e.in_pits),
+                ),
+                GameSource::new_number(
+                    uuid!("8cc69329-3dbd-4283-8c9f-38ce4cceef46"),
+                    "Gear",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.gear as f32)
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("f5cfbdd0-bc01-4c6c-9d80-5b8b0db8cf8e"),
+                    "Speed m/s",
+                    |_: &ValueStore, context: ModelContext<'_>| context.entry.map(|e| *e.speed),
+                ),
+                GameSource::new_number(
+                    uuid!("095c3af8-3971-4bc9-982b-cc1dd7a3166f"),
+                    "Speed km/h",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.speed * 3.6)
+                    },
+                ),
+                GameSource::new_number(
+                    uuid!("32145d78-3b0a-4108-ba9d-505f54a8e7bc"),
+                    "Speed mp/h",
+                    |_: &ValueStore, context: ModelContext<'_>| {
+                        context.entry.map(|e| *e.speed * 2.23694)
+                    },
+                ),
+                GameSource::new_bool(
+                    uuid!("f4c61514-a6f3-4f11-a70e-9743b387cb8e"),
+                    "Connected",
+                    |_: &ValueStore, context: ModelContext<'_>| context.entry.map(|e| *e.connected),
+                ),
+                GameSource::new_bool(
+                    uuid!("de909160-f54b-40cf-a987-6a8453df0914"),
+                    "Is focused",
+                    |_: &ValueStore, context: ModelContext<'_>| context.entry.map(|e| e.focused),
                 ),
                 GameSource::new_number(
                     uuid!("4d519d42-52e9-435c-b614-8d70b42ed3b0"),
